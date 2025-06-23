18d4f4aba6faf31a3701d5df0cbd41555e988853
DipeshOmre <dipesh@gmail.com>
2025-06-23 13:05:16 +0530

    after block design

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Manager = () => {
     return (
         <>
-            <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-slate-400 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
             </div>
-            <div className=" bg-slate-50 mycontainer">
+            <div className="  mycontainer">
                 <h1 className='text-4xl font-bold text-center'><span className='text-green-700'>
                     &lt;
                 </span>
@@ -15,10 +15,10 @@ const Manager = () => {
                     </span></h1>
                 <p className='text-green-900 text-lg text-center'>Your name password manager</p>
                 <div className='flex flex-col p-4 gap-8 items-center'>
-                    <input className='rounded-full border border-green-500 w-full text-black p-4 py-1' type="text" name='' id='' />
+                    <input className='rounded-full border border-black-500 w-full text-black p-4 py-1' type="text" name='' id='' />
                     <div className="flex w-full justify-between gap-8">
-                        <input className='rounded-full border border-green-500 w-full text-black p-4 py-1' type="text" name='' id='' />
-                        <input className='rounded-full border border-green-500 w-full text-black p-4 py-1' type="text" name='' id='' />
+                        <input className='rounded-full border border-black-500 w-full text-black p-4 py-1' type="text" name='' id='' />
+                        <input className='rounded-full border border-black-500 w-full text-black p-4 py-1' type="text" name='' id='' />
                     </div>
                     <button className='flex items-center justify-center bg-green-500 px-4 py-2
                     rounded-full w-fit text-center hover:bg-green-400'>
